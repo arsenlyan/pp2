ab2e29ad202763433a319f00aef5bcd739472009
arsenlyan <157819046+arsenlyan@users.noreply.github.com>
2024-04-27 01:18:13 +0500

    Add files via upload

---
+++ lab10/4.py
@@ -0,0 +1,31 @@
+#Querying data from the tables (with different filters)
+import psycopg2
+
+conn = psycopg2.connect(
+	database="postgres",
+	user='postgres',
+	password='Dulat2005',
+	host='localhost',
+	#port='5432'
+)
+cursor = conn.cursor()
+conn.autocommit = True
+
+#select all
+#sql = f"select * from phonebook";
+
+#select filter 
+sql = f"select * from phonebook where first_name = \'timon\' ";
+
+
+#select with sort filter decrease by first
+#sql = f"select * from phonebook by order by first_name desc";
+
+
+#select with sort filter increase by first
+#sql = f"select * from phonebook by order by first_name asc";
+
+
+cursor.execute(sql)
+info = cursor.fetchall()
+print(info)
